2ebe14853603f2749bf867c481755d8ee22339ec
Pavan Baddi <pavanbaddi911@gmail.com>
2020-04-09 17:25:31 +0530

    minor

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\CountryPopulationModel;
+use App\Jobs\ProcessCountriesPopulation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CountryController extends Controller
 {
-    //
+    public function process(Request $request){
+        Log::info('Entered Job CountryController process method');
+
+        $country = CountryPopulationModel::find(101);
+        
+        ProcessCountriesPopulation::dispatch($country);
+        
+        Log::info('Exited Job CountryController process method');
+    }
 }
--- app/Jobs/ProcessCountriesPopulation.php
+++ app/Jobs/ProcessCountriesPopulation.php
@@ -2,24 +2,30 @@
 
 namespace App\Jobs;
 
+use App\CountryPopulationModel;
+use App\CountryStatePopulationModel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class ProcessCountriesPopulation implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $country_census;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(CountryPopulationModel $country)
     {
-        //
+        Log::info('Entered Job ProcessCountriesPopulation __constructor method');
+        $this->country_census = $country;
     }
 
     /**
@@ -29,6 +35,13 @@ class ProcessCountriesPopulation implements ShouldQueue
      */
     public function handle()
     {
-        //
+        Log::info('Entered Job ProcessCountriesPopulation handle method');
+        $country_states_population_sum =  CountryStatePopulationModel::where(["country_id" => $this->country_census["country_id"]])->sum('state_population');
+        
+        $country = CountryPopulationModel::find($this->country_census["country_id"]); 
+        $country->total_population = $country_states_population_sum;
+        $country->save();
+
+        Log::info('Exited from Job ProcessCountriesPopulation handle method');
     }
 }
