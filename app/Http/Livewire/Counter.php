834db80efd07625803079920999871db2c6da668
Pavan Baddi <pavanbaddi911@gmail.com>
2020-05-09 16:35:58 +0530

    Completed livewire Counter application

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -3,9 +3,38 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Log;
 
 class Counter extends Component
 {
+
+    public $initial = 0;
+
+    public $starting_counter_number=NULL;
+
+    public function increament(){
+        (int)$this->initial+=1;
+    }
+
+    public function decreament(){
+        if($this->initial>0){
+            $this->initial-=1;
+        }else{
+            session()->flash('info', "You cannot have negative value in counter");
+        }
+    }
+
+    public function counterForm(){
+
+        $validated_data = $this->validate([
+            "starting_counter_number" => "required|integer|gte:0",
+        ]);
+
+        $num = $validated_data["starting_counter_number"];
+
+        $this->initial = $num;
+    }
+
     public function render()
     {
         return view('livewire.counter-views.counter'); 
--- resources/views/livewire/counter-views/counter.blade.php
+++ resources/views/livewire/counter-views/counter.blade.php
@@ -1,3 +1,24 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
+    <h2>Laravel Livewire Counter Web Application</h2>
+
+    @if ( session()->has('info') )
+        <p><strong>Info : </strong> {{ session('info') }}</p>
+    @endif
+
+    <button type="button" wire:click="increament" >Increament</button>
+    <input type="number" wire:model="initial" value="" readonly >
+    <button type="button" wire:click="decreament" >Decrement</button>
+
+    <hr>
+
+    <form wire:submit.prevent="counterForm">
+        <label for="">Enter Counter Starting Number</label><br>
+        <input type="text" wire:model.lazy="starting_counter_number" value=""><br>
+        @error('starting_counter_number')
+            <span style="color:red">{{ $message }}</span> 
+        @enderror
+        <br>
+        <input type="submit" value="Submit">
+    </form>
 </div>
