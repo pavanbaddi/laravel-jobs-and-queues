0f9e708390ae15ed317ea6fbddf23635a2254212
Pavan Baddi <pavanbaddi911@gmail.com>
2020-05-15 15:31:42 +0530

    mior

--- resources/views/livewire/files/base.blade.php
+++ resources/views/livewire/files/base.blade.php
@@ -23,8 +23,6 @@
         {# livewire('Files.FileUpload') #} this does not work
      -->
     
-    
-
     <script src="{{ url('assets/js/jquery.min.js') }}"></script>
     <script src="{{ url('assets/js/popper.min.js') }}"></script>
     <script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
