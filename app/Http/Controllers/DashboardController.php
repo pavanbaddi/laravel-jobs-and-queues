ce5aac74d691a832fc6734d44531323f25e6bb9d
Pavan Baddi <pavanbaddi911@gmail.com>
2020-06-02 10:11:38 +0530

    minor

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(Request $request){
-        dd($request->user);
+        // dd($request->user);
     }
 }
