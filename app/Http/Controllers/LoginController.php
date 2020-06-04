5d0832fc45ccb67aea93225e4089f41f15067eb5
Pavan Baddi <pavanbaddi911@gmail.com>
2020-06-04 09:02:04 +0530

    minor

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,6 +26,7 @@ class LoginController extends Controller
 
         // dd($remember_me);
 
+        
         if(Auth::attempt($credential)){
             $user = User::where(["email" => $credential['email']])->first();
             
