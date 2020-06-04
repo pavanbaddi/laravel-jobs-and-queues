74436b4c1dc2e67b3a93ebc8ea33e7640d1c79de
Pavan Baddi <pavanbaddi911@gmail.com>
2020-06-04 12:53:00 +0530

    minor

--- resources/views/turbolinks/dashboard.blade.php
+++ resources/views/turbolinks/dashboard.blade.php
@@ -1,8 +1,14 @@
 @extends('turbolinks.base')
 
+<link rel="stylesheet" href="{{ url('turbolinks-assets/css/dashboard.css') }}">
+
 @section('content')
-<h2>Dashboard</h2>
+
+
+<h2 class="dashboard-title" >Dashboard</h2>
 
 <a href="{{ url('turbolinks') }}">goto login</a>
 
 @endsection
+
+<!-- <script src="{{ url('turbolinks-assets/js/dashboard.js') }}" ></script> -->
